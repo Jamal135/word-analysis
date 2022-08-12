3a740cfac9267dab1575b3d2a69813fe47c1b006
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2022-08-13 02:06:07 +1000

    Updated to store dataframe for frequency analysis

--- word_analysis.py
+++ word_analysis.py
@@ -1,20 +1,25 @@
-from pandas import DataFrame, Series
-from collections import defaultdict
+''' Created: 13/08/2022 '''
 
-def load_words(txt_name: str):
+from pandas import DataFrame, Series, read_csv
+from collections import defaultdict
+from tqdm import tqdm
+
+
+def load_words(textfile_name: str):
     """ Returns: Loaded list of words from txt. """
-    textfile = open(f"{txt_name}.txt", "r")
+    textfile = open(textfile_name, "r")
     wordlist = []
-    for line in textfile:
+    for line in tqdm(textfile):
         stripped_line = line.strip()
         wordlist.append(stripped_line)
     textfile.close
-    return wordlist
+    return set(wordlist)
+
 
 def dictionary_build(wordlist: list):
     ''' Returns: Dictionary tree next letter frequency. '''
     data = defaultdict(int)
-    for word in wordlist:
+    for word in tqdm(wordlist):
         for position, letter in enumerate(word):
             previous = word[:position + 1]
             length = len(word)
@@ -22,6 +27,7 @@ def dictionary_build(wordlist: list):
                 segment = previous[point:position]
                 data[length, position, segment, letter] += 1
     return data
+
 
 def dataframe_build(data: dict):
     ''' Returns: Built and named dataframe from dictionary. '''
@@ -31,18 +37,6 @@ def dataframe_build(data: dict):
                               0: 'Count'}, inplace=True)
     return dataframe
 
-def dataframe_extract(dataframe, length: list, position: list, previous: list):
-    ''' Returns: Dataframe cut to desired rows. '''
-    if not length:
-        length = dataframe["Length"].unique()
-    if not position:
-        position = dataframe["Position"].unique()
-    if not previous:
-        previous = dataframe["Previous"].unique()
-    dataframe = dataframe[dataframe["Length"].isin(length) &
-                          dataframe["Position"].isin(position) &
-                          dataframe["Previous"].isin(previous)]
-    return dataframe
 
 def frequency(dataframe):
     ''' Returns: Sorted count and probability of each letter. '''
@@ -57,10 +51,53 @@ def frequency(dataframe):
         letter_data[letter] += round((1/total_count)*count, 4)
     return (sorted(letter_data.items(), key = lambda ab:(ab[1], ab[0]), reverse=True)) 
 
-def word_analysis(length: list, position: list, previous: list, txt_name: str = "cl"):
-    ''' Returns: The percentage chance of each next letter given arguments. '''
-    wordlist = load_words(txt_name)
+
+def build_dataframe(output_name: str, textfile_name: str = None):
+    """ Returns: Created CSV file frequency analysis given textfile. """
+    if textfile_name is None:
+        textfile_name = output_name
+    if not textfile_name.endswith(".txt"):
+        textfile_name += ".txt"
+    print(f"Loading Words: {textfile_name}")
+    wordlist = load_words(textfile_name)
     word_dictionary = dictionary_build(wordlist)
     dataframe = dataframe_build(word_dictionary)
+    if not output_name.endswith(".csv"):
+        output_name += ".csv"
+    print(f"Saving Dataframe: {output_name}")
+    dataframe.to_csv(output_name, index=False)
+
+
+build_dataframe("words_short")
+build_dataframe("words_long")
+
+
+def load_CSV(filename: str):
+    ''' Returns: CSV loaded to dataframe. '''
+    if not filename.endswith(".csv"):
+        filename += ".csv"
+    return read_csv(filename)
+
+
+def dataframe_extract(dataframe, length: list, position: list, previous: list):
+    ''' Returns: Dataframe cut to desired rows. '''
+    if not length:
+        length = dataframe["Length"].unique()
+    if not position:
+        position = dataframe["Position"].unique()
+    if not previous:
+        previous = dataframe["Previous"].unique()
+    dataframe = dataframe[dataframe["Length"].isin(length) &
+                          dataframe["Position"].isin(position) &
+                          dataframe["Previous"].isin(previous)]
+    return dataframe
+
+
+def word_analysis(length: list, position: list, previous: list, datafile: str = "words_short"):
+    ''' Returns: The percentage chance of each next letter given arguments. '''
+    dataframe = load_CSV(datafile)
     cut_dataframe = dataframe_extract(dataframe, length, position, previous)
-    return frequency(cut_dataframe)
+    print(frequency(cut_dataframe))
+
+
+word_analysis([], [], ["a", "b"], "words_long")
