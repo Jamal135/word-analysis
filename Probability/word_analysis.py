7aabfd52617b18e6a9061981f8de5e0ce1046153
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2022-08-13 17:22:24 +1000

    Added shorter dictionary, other bug fixes

--- Probability/word_analysis.py
+++ Probability/word_analysis.py
@@ -1,6 +1,5 @@
 ''' Creation Date: 27/10/2021 '''
 
-from distutils.command.build import build
 from pandas import DataFrame, Series, read_csv
 from collections import defaultdict
 from tqdm import tqdm
@@ -8,7 +7,7 @@ from tqdm import tqdm
 
 def load_words(textfile_name: str):
     """ Returns: Loaded list of words from txt. """
-    textfile = open(textfile_name, "r")
+    textfile = open(f"Probability/{textfile_name}", "r")
     wordlist = []
     for line in tqdm(textfile):
         stripped_line = line.strip()
@@ -17,7 +16,7 @@ def load_words(textfile_name: str):
     return wordlist
 
 
-def dictionary_build(wordlist: list):
+def dictionary_build(wordlist: list, next: bool):
     ''' Returns: Dictionary tree next letter frequency. '''
     data = defaultdict(int)
     for word in tqdm(wordlist):
@@ -28,36 +27,30 @@ def dictionary_build(wordlist: list):
             following = word[index + 1:]
             for cut in range(len(previous) + 1):
                 a = previous[cut:]
-                for cut in range(len(following) + 1):
-                    b = following[:cut]
-                    data[length, index, a, b, letter] += 1
+                if next:
+                    for cut in range(len(following) + 1):
+                        b = following[:cut]
+                        data[length, index, a, b, letter] += 1
+                else:
+                    data[length, index, a, letter] += 1
     return data
 
 
-def dataframe_build(data: dict):
+def dataframe_build(data: dict, next: bool):
     ''' Returns: Built and named dataframe from dictionary. '''
     dataframe = DataFrame.from_records(Series(data).reset_index())
-    dataframe.rename(columns={'level_0': 'Length', 'level_1': 'Position',
-                              'level_2': 'Previous', 'level_3': 'Following',
-                              'level_4': 'Letter', 0: 'Count'}, inplace=True)
+    if next:
+        dataframe.rename(columns={'level_0': 'Length', 'level_1': 'Position',
+                                'level_2': 'Previous', 'level_3': 'Following',
+                                'level_4': 'Letter', 0: 'Count'}, inplace=True)
+    else:
+        dataframe.rename(columns={'level_0': 'Length', 'level_1': 'Position',
+                                'level_2': 'Previous', 'level_3': 'Letter', 
+                                0: 'Count'}, inplace=True)
     return dataframe
 
 
-def frequency(dataframe):
-    ''' Returns: Sorted count and probability of each letter. '''
-    characters = "abcdefghijklmnopqrstuvwxyz"
-    letter_data = defaultdict(int)
-    total_count = dataframe["Count"].sum()
-    if total_count == 0: 
-        return None
-    for letter in characters:
-        rows = dataframe[dataframe["Letter"].isin([letter])]
-        count = sum(rows['Count'])
-        letter_data[letter] += round((1/total_count)*count, 4)
-    return (sorted(letter_data.items(), key = lambda ab:(ab[1], ab[0]), reverse=True)) 
-
-
-def build_dataframe(output_name: str, textfile_name: str = None):
+def build_dataframe(output_name: str, textfile_name: str = None, next: bool = True):
     """ Returns: Created CSV file frequency analysis given textfile. """
     if textfile_name is None:
         textfile_name = output_name
@@ -65,19 +58,19 @@ def build_dataframe(output_name: str, textfile_name: str = None):
         textfile_name += ".txt"
     print(f"Loading Words: {textfile_name}")
     wordlist = load_words(textfile_name)
-    word_dictionary = dictionary_build(wordlist)
-    dataframe = dataframe_build(word_dictionary)
+    word_dictionary = dictionary_build(wordlist, next)
+    dataframe = dataframe_build(word_dictionary, next)
     if not output_name.endswith(".csv"):
         output_name += ".csv"
     print(f"Saving Dataframe: {output_name}")
-    dataframe.to_csv(output_name, index=False)
+    dataframe.to_csv(f"Probability/{output_name}", index=False)
 
 
 def load_CSV(filename: str):
     ''' Returns: CSV loaded to dataframe. '''
     if not filename.endswith(".csv"):
         filename += ".csv"
-    return read_csv(filename)
+    return read_csv(f"Probability/{filename}")
 
 
 def dataframe_extract(dataframe, length: list, position: list, 
@@ -98,6 +91,20 @@ def dataframe_extract(dataframe, length: list, position: list,
     return dataframe
 
 
+def frequency(dataframe):
+    ''' Returns: Sorted count and probability of each letter. '''
+    characters = "abcdefghijklmnopqrstuvwxyz"
+    letter_data = defaultdict(int)
+    total_count = dataframe["Count"].sum()
+    if total_count == 0: 
+        return None
+    for letter in characters:
+        rows = dataframe[dataframe["Letter"].isin([letter])]
+        count = sum(rows['Count'])
+        letter_data[letter] += round((1/total_count)*count, 4)
+    return (sorted(letter_data.items(), key = lambda ab:(ab[1], ab[0]), reverse=True)) 
+
+
 def word_analysis(length: list, position: list, previous: list, following: list, 
                   datafile: str = "words_short"):
     ''' Returns: The percentage chance of each next letter given arguments. '''
@@ -107,4 +114,4 @@ def word_analysis(length: list, position: list, previous: list, following: list,
 
 
 # Find probability letters given any position, any length where previous chars are a & b and following is a.
-word_analysis([], [], ["a", "b"], ["a"], "words_long")
+word_analysis([3], [1], ["t"], [], "words_long")
